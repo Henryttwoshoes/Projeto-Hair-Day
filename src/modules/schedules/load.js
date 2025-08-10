e88e60f1aac14b8daab5cdf2f471a9a1fe925bac
Henrique de Castro Santos <henryttwoshoes@gmail.com>
2025-08-10 11:22:11 -0300

    aula 25 feita

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,15 +1,20 @@
+import { scheduleFetchByDay} from "../../services/schedule-fetch-by-day.js"
 import { hoursLoad } from "../form/hours-load.js"
 
 // Seleciona o input de data.
 const selectedDate = document.getElementById("date")
 
 
-export function schedulesDay(){
+export async function schedulesDay(){
     // Busca na API os agendamentos para carregar do lado direito da tela
     // Renderiza as horas disponíveis.
 
     //Obtém a data do input
     const date = selectedDate.value
+
+    // Busca na API os agendamentos
+    const DailySchedules = await scheduleFetchByDay({ date })
+    console.log(DailySchedules)
 
 
 
